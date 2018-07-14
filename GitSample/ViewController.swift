938e38ef5d2bc11ece29a1b90e55d5de51d2b7ef
Puneet Kumar <puneetkumar@puneets-macbook-pro.local>
2018-07-14 17:05:40 +0530

    first commit by puneet

--- GitSample/ViewController.swift
+++ GitSample/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print("First Commit by puneet")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
